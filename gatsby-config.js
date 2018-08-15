9df3f6f37027c90f4453c8ce6bdd3495598d42b1
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-08-14 23:35:54 -0700

    Add gatsby sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'The After Hours',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sass`,
+  ],
 }
